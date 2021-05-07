c7dbcfcbea1fce493fca3d082a5317d8b1d72ec8
Gonzalo Lopez Cabello <82189704+GnzLC@users.noreply.github.com>
2021-05-06 22:10:02 -0300

    Add files via upload

--- 01-EjemploTipos/asserts.cpp
+++ 01-EjemploTipos/asserts.cpp
@@ -13,31 +13,52 @@ int main(){
     assert(true and true);
     assert(true or false);
     assert(not false);
+
     //char
-    char A;
-    char a;
-    assert(a==a);
-    assert(A!=a);
-    assert(int(A)<=int(a));
-    assert(int(a)>=int(A));
+    assert('a'=='a');
+    assert('A'!='a');
+    assert('A'<='a');
+    assert('a'>='A');
+    assert('A'+1 == 'B');
     //string
-    string h = "hola";
-    string m = " mundo";
-    string hm = "hola mundo";
-    assert(h.length() == 4);
-    assert(h+m ==hm);
-    assert(h != m);
-    assert(h <= hm);
-    assert(hm >= m);
+
+    assert("hola" == "hola");
+    assert("hola" != "chau");
+    assert(string("hola")+ string(" mundo") =="hola mundo");
+    assert(string("hola").length()==4);
+
     //int
-    assert(2+2==4);
-    assert(4*4==16);
-    assert(10%3==1);
+    assert(1 == 1);
     assert(1 != 0);
-    assert(64/8==8);
+    assert(1 >= 0);
+    assert(0 <= 1);
+    assert(2+2==4);
     assert(100-99 == 1);
-    assert(2 - 3 == -1);
+    assert(4*4==16);
+    assert(64/8==8);
+    assert(10%3==1);
+
     //double
-    assert(-0.3 <= -0.33);
+    assert(1.0 == 1.0);
+    assert(1.0 != 2.0);
+    assert(1.0 >= 0.0);
+    assert(0.0 <= 1.0);
+    assert(2.5+2.5==5.0);
+    assert(3.5-2.5==1.0);
+    assert(4.0*4.0==16.0);
+    assert(64.0/8.0==8.0);
+
+    //unsigned 
+    assert(1u == 1u);
+    assert(1u != 0u);
+    assert(1u >= 0u);
+    assert(0u <= 1u);
+    assert(2u+2u==4u);
+    assert(100u-99u == 1u);
+    assert(4u*4u==16u);
+    assert(64u/8u==8u);
+    assert(10u%3u==1u);
+
+
     printf ("Todo bien\n");
 }
